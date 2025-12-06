015a6d2c6dc485fe026fe5e8c1ba3b30950619f2
Finian Doonan <162761274+fin81985@users.noreply.github.com>
2025-12-06 13:43:42 +0000

    assign02check

--- assignment02-bankholdiays.py
+++ assignment02-bankholdiays.py
@@ -1,17 +1,19 @@
 # Bank holidays in the northern Ireland
 # Author: Finian Doonan
 
-import requests
+import requests # https://requests.readthedocs.io/en/latest/
 
 url = "https://www.gov.uk/bank-holidays.json"
 response = requests.get(url)
-data = response.json()
-print (data)
+data = response.json() # downloads bank holiday data in JSON format
+
+year = 2026 # specify year
 
 
 # northern ireland bank holidays
 for event in data["northern-ireland"]["events"]:
-    #print (f"{event}")
-    print (f"{event['title']} on {event['date']}")
+    if event['date'].startswith(str(year)):# check for specified year
+        
+        print(f"{event['title']} on {event['date']}")# prints bank holidays for the specified year
 
 
